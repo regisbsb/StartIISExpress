7b4b8fe601dac3d8bfc91ea30997f0ce6e887ff4
Regis Bittencourt <rbittencourt@dctfh.com>
2014-03-11 10:06:05 +0000

    Real first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+
+namespace StartIISExpress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = @"C:\Program Files (x86)\IIS Express\iisexpress.exe",
+                    RedirectStandardError = true,
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                    Arguments = string.Format("/site:{0}", args[0])
+                }
+            };
+            process.Start();
+        }
+    }
+}
